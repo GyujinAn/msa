6a30c09da4dd52f1e23e79c119265e28db1f47f5
Gyujin An <agj017@gmail.com>
2022-02-21 18:28:34 +0900

    create portal-user

---
+++ portal-user/src/test/java/cloudmanagementplatform/portaluser/PortalUserApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudmanagementplatform.portaluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
